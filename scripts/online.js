d9e8f84db6c63dc096ccf6d959521542e8e4dade
Igor Zubkov <zubkov.igorv@gmail.com>
2024-11-12 12:13:00 +0700

    button start and stop

--- scripts/online.js
+++ scripts/online.js
@@ -16,6 +16,7 @@ const {
     Tooltip
 } = chartjs;
 Chart.register([LinearScale, LineController, CategoryScale, PointElement, LineElement, Legend, Tooltip]);
+
 let onlineChart;
 let chartConfig = {};
 const configFilePath = path.join(__dirname, 'settings/settings.json');
@@ -23,6 +24,8 @@ const time = [];
 const datasets = [];
 let isChartRunning = false; // Флаг для отслеживания состояния графика
 const statusButton = document.getElementById('statusButton');
+let csvFilePath = ''; // Переменная для хранения пути к CSV-файлу
+
 // Загрузка конфигурации графиков
 try {
     const configData = fs.readFileSync(configFilePath, 'utf-8');
@@ -30,6 +33,7 @@ try {
 } catch (error) {
     console.error('Ошибка загрузки конфигурации графиков:', error);
 }
+
 // Функция для обновления цвета кнопки
 function updateButtonColor(isConnected) {
     statusButton.style.background = isConnected 
@@ -37,10 +41,11 @@ function updateButtonColor(isConnected) {
         : 'radial-gradient(farthest-side at top left, #ff6347, #ffa494)';
     statusButton.disabled = !isConnected;
 }
+
 // Подключение к устройству Modbus TCP
 async function connectModbus() {
     try {
-        await client.connectTCP("localhost", { port: 502 });
+        await client.connectTCP("186.168.65.6", { port: 502 });
         client.setID(1);
         updateButtonColor(true);
     } catch (error) {
@@ -48,6 +53,7 @@ async function connectModbus() {
         updateButtonColor(false);
     }
 }
+
 // Проверка соединения каждые 5 секунд
 setInterval(async () => {
     try {
@@ -61,8 +67,10 @@ setInterval(async () => {
         connectModbus();
     }
 }, 5000);
+
 // Инициируем подключение
 connectModbus();
+
 async function readModbusData(client, address) {
     try {
         const data = await client.readHoldingRegisters(address, 2);
@@ -77,6 +85,7 @@ async function readModbusData(client, address) {
         return null;
     }
 }
+
 // Функция для обновления графика с данными Modbus 
 async function updateChartWithModbusData(chart, client) {
     const dataMap = {
@@ -111,10 +120,12 @@ async function updateChartWithModbusData(chart, client) {
     updateChartAxes(chart);
     chart.update();
 }
+
 // Функция для проверки, следует ли рисовать график
 function shouldDrawChart(chartName) {
     return chartConfig[chartName] && chartConfig[chartName].active === 1;
 }
+
 // Функция для создания осей Y на основе конфигурации
 function createYAxis(chart, chartName) {
     const max = chartConfig[chartName].max || 100;
@@ -133,6 +144,7 @@ function createYAxis(chart, chartName) {
         }
     };
 }
+
 // Обновление осей Y на основе активных графиков
 function updateChartAxes(chart) {
     chart.options.scales = {}; // Очищаем текущие оси 
@@ -143,11 +155,21 @@ function updateChartAxes(chart) {
     }
     chart.update();
 }
-// Функция для старта обновления графика
+
+let updateInterval; // Declare a variable to hold the interval ID
+
+// Обработчик события "Старт"
 function startChart() {
     if (!isChartRunning) {
         isChartRunning = true;
-        setInterval(async () => {
+
+        // Делаем input и кнопку "Применить" неактивными
+        const inputs = document.querySelectorAll('input');
+        inputs.forEach(input => input.disabled = true);
+        document.getElementById('apply').disabled = true;
+
+        // Start the interval and store its ID
+        updateInterval = setInterval(async () => {
             const currentTime = new Date().toLocaleTimeString();
             onlineChart.data.labels.push(currentTime);
             let shouldUpdateChart = false;
@@ -165,6 +187,7 @@ function startChart() {
         }, 1000);
     }
 }
+
 // Обработчик события DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
     const ctx = document.getElementById('online').getContext('2d');
@@ -185,31 +208,8 @@ document.addEventListener('DOMContentLoaded', () => {
             scales: {}
         }
     });
-    // Привязка функции к кнопке
     document.getElementById('startButton').addEventListener('click', startChart);
 });
-// Функция для преобразования данных в формат CSV
-
-function convertToCSV() {
-    // Проверяем, существуют ли данные
-    if (!onlineChart || !onlineChart.data || !onlineChart.data.labels || !onlineChart.data.datasets) {
-        throw new Error('Данные для преобразования в CSV отсутствуют');
-    }
-    // Определяем заголовки CSV
-    const headers = ['Client', 'bush', 'well', 'name_work'];
-    // Создаем строки для данных
-    const rows = onlineChart.data.labels.map((label, index) => {
-        const row = [];
-        // Извлекаем данные по каждому заголовку
-        headers.forEach(header => {
-            const dataset = onlineChart.data.datasets.find(ds => ds.label === header);
-            row.push(dataset ? dataset.data[index] || '' : ''); // Добавляем значение или пустую строку
-        });
-        return row.join(','); // Преобразуем строку в формат CSV
-    });
-    // Объединяем заголовки и строки в одну строку CSV
-    return [headers.join(','), ...rows].join('\n');
-}
 
 // Функция для преобразования данных в формат CSV
 function convertInputsToCSV() {
@@ -226,55 +226,116 @@ function convertInputsToCSV() {
     // Получаем текущую дату в формате DD:MM:YY
     const currentDate = new Date();
     const day = String(currentDate.getDate()).padStart(2, '0');
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
-    const year = String(currentDate.getFullYear()).slice(-2); // Берем последние 2 цифры года
-    const formattedDate = `${day}:${month}:${year}`; // Форматируем дату
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const year = String(currentDate.getFullYear()).slice(-2);
+    const formattedDate = `${day}:${month}:${year}`;
 
     // Создаем массив строк CSV
     const csvData = [
-        ['Заказчик', 'Куст', 'Скважина', 'Название работы', 'Дата'], // Добавляем заголовок для даты
-        [client, bush, well, nameWork, formattedDate] // Добавляем данные с текущей датой
+        ['Заказчик', 'Куст', 'Скважина', 'Название работы', 'Дата'],
+        [client, bush, well, nameWork, formattedDate]
     ];
     
-    // Преобразуем массив в строку CSV
     return csvData.map(row => row.join(',')).join('\n');
 }
 
 // Объявление функции
 async function saveChartDataToCSV() {
-    const csvData = convertInputsToCSV(); // Получаем данные CSV или null, если есть ошибка
+    const csvData = convertInputsToCSV();
     
-    // Проверяем, есть ли сообщение об ошибке
     if (!csvData) {
         const messageElement = document.getElementById('message');
-        messageElement.textContent = 'Не все поля заполнены, файл не создан.'; // Выводим сообщение об ошибке
+        messageElement.textContent = 'Не все поля заполнены, файл не создан.';
         messageElement.style.color = 'red';
         messageElement.style.display = 'inline';
-        return; // Выходим из функции, если есть ошибка
+        return;
     }
 
     try {
-        const filePath = await ipcRenderer.invoke('show-save-dialog'); // Открываем диалог для сохранения файла
-        
-        if (filePath) {
-            ipcRenderer.send('save-csv', { filePath, csvData }); // Отправляем данные для сохранения
-            const messageElement = document.getElementById('message');
-            messageElement.textContent = `Файл успешно создан: ${filePath}`; // Сообщение об успешном сохранении
-            messageElement.style.color = 'green';
-            messageElement.style.display = 'inline';
-        } else {
-            const messageElement = document.getElementById('message');
-            messageElement.textContent = 'Не удалось сохранить файл.'; // Сообщение, если файл не был сохранен
-            messageElement.style.color = 'red';
-            messageElement.style.display = 'inline';
+        const userSelectedPath = await ipcRenderer.invoke('show-save-dialog');
+        if (userSelectedPath) {
+            csvFilePath = userSelectedPath;
+            console.log('Путь к файлу установлен:', csvFilePath);
         }
+
+        // Сохраняем данные в csvFilePath
+        ipcRenderer.send('save-csv', { filePath: csvFilePath, csvData });
+        const messageElement = document.getElementById('message');
+        messageElement.textContent = `Файл успешно создан: ${csvFilePath}`;
+        messageElement.style.color = 'green';
+        messageElement.style.display = 'inline';
     } catch (error) {
         const messageElement = document.getElementById('message');
-        messageElement.textContent = `Ошибка: ${error.message}`; // Выводим сообщение об ошибке
-        messageElement.style.color = 'red'; // Устанавливаем цвет текста в красный
-        messageElement.style.display = 'inline'; // Делаем элемент видимым
+        messageElement.textContent = `Ошибка: ${error.message}`;
+        messageElement.style.color = 'red';
+        messageElement.style.display = 'inline';
     }
 }
+
+
+// Функция для записи данных в CSV
+async function writeDataToCSV(dataToWrite) {
+    try {
+        // Check if the file path is set
+        if (!csvFilePath) {
+            throw new Error('Путь к файлу не установлен.');
+        }
+
+        // Write the CSV data to the specified file
+        fs.appendFile(csvFilePath, dataToWrite + '\n', 'utf8', (err) => {
+            if (err) {
+                console.error('Ошибка при записи в файл:', err);
+                throw err;
+            }
+        });
+    } catch (error) {
+        console.error('Ошибка в writeDataToCSV:', error);
+        throw error; // Rethrow the error to be handled in the calling function
+    }
+}
+
+// Обработчик события "Стоп"
+document.getElementById('stopButton').addEventListener('click', async () => {
+    if (isChartRunning) {
+        // Останавливаем обновление графика
+        isChartRunning = false;
+
+        // Clear the interval to stop chart updates
+        clearInterval(updateInterval);
+
+        // Восстанавливаем активность input и кнопки "Применить"
+        const inputs = document.querySelectorAll('input');
+        inputs.forEach(input => input.disabled = false);
+        document.getElementById('apply').disabled = false;
+
+        // Записываем данные в заранее указанный CSV-файл
+        const dataToWrite = onlineChart.data.labels.map((label, index) => {
+            const row = [label];
+            onlineChart.data.datasets.forEach(dataset => {
+                row.push(dataset.data[index] !== undefined ? dataset.data[index] : '');
+            });
+            return row.join(',');
+        }).join('\n');
+
+        if (dataToWrite.trim() === '') {
+            console.log('Нет данных для записи в файл.');
+            return;
+        }
+
+        await writeDataToCSV(dataToWrite);
+
+        // Отображаем сообщение о сохранении данных на экране
+        const messageElement = document.getElementById('message');
+        messageElement.textContent = `Данные успешно сохранены в файл: ${csvFilePath}`;
+        messageElement.style.color = 'green';
+        messageElement.style.display = 'inline';
+
+        // Добавляем таймер для скрытия сообщения через 5 секунд
+        setTimeout(() => {
+            messageElement.style.display = 'none';
+        }, 50000);
+    }
+});
 
 // Добавление обработчика события
 document.getElementById('apply').addEventListener('click', saveChartDataToCSV);
@@ -293,5 +354,3 @@ ipcRenderer.on('display-message', (event, message, filePath) => {
         }, 500);
     }, 60000);
 });
-// Привязка функции к кнопке сохранения
-document.getElementById('saveCsvButton').addEventListener('click', saveChartDataToCSV);
